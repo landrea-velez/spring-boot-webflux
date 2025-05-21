76235a1820b90ce5be4e9c29de2876d76b4628c4
landrea-velez <landrea.velez@gmail.com>
2025-05-21 16:58:00 -0500

    Contrapresion datadriver

--- src/main/java/com/landreavelez/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/landreavelez/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,15 @@
 package com.landreavelez.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
+
 import reactor.core.publisher.Flux;
 
 import com.landreavelez.springboot.webflux.app.models.dao.ProductoDao;
@@ -33,4 +37,21 @@ public class ProductoController {
 		model.addAttribute("titulo", "Listado de Productos");
 		return "listar";
 	};
+	
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos, 1));
+		model.addAttribute("titulo", "Listado de productos");
+		return "listar";
+	}
 }
