353980428a3a295bb62779c6088730cb998bf101
landrea-velez <landrea.velez@gmail.com>
2025-06-04 14:06:43 -0500

    ajustando métodos en el controlador

--- src/main/java/com/landreavelez/springboot/webflux/app/models/services/ProductoServiceImpl.java
+++ src/main/java/com/landreavelez/springboot/webflux/app/models/services/ProductoServiceImpl.java
@@ -1,65 +1,72 @@
 package com.landreavelez.springboot.webflux.app.models.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.landreavelez.springboot.webflux.app.models.dao.ProductoDao;
+import com.landreavelez.springboot.webflux.app.models.dao.CategoriaDao;
 import com.landreavelez.springboot.webflux.app.models.documents.Categoria;
 import com.landreavelez.springboot.webflux.app.models.documents.Producto;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Service
 public class ProductoServiceImpl implements ProductoService{
-
+	
+	@Autowired
+	private ProductoDao dao;
+	
+	@Autowired
+	private CategoriaDao categoriaDao;
+	
 	@Override
 	public Flux<Producto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Flux<Producto> findAllConNombreUpperCase() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Flux<Producto> findAllConNombreUpperCaseRepeat() {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.findAll();
 	}
 
 	@Override
 	public Mono<Producto> findById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.findById(id);
 	}
 
 	@Override
 	public Mono<Producto> save(Producto producto) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.save(producto);
 	}
 
 	@Override
 	public Mono<Void> delete(Producto producto) {
-		// TODO Auto-generated method stub
-		return null;
+		return dao.delete(producto);
+	}
+
+	@Override
+	public Flux<Producto> findAllConNombreUpperCase() {
+		return dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+	}
+
+	@Override
+	public Flux<Producto> findAllConNombreUpperCaseRepeat() {
+		return findAllConNombreUpperCase().repeat(5000);
 	}
 
 	@Override
 	public Flux<Categoria> findAllCategoria() {
-		// TODO Auto-generated method stub
-		return null;
+		return categoriaDao.findAll();
 	}
 
 	@Override
 	public Mono<Categoria> findCategoriaById(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return categoriaDao.findById(id);
 	}
 
 	@Override
 	public Mono<Categoria> saveCategoria(Categoria categoria) {
-		// TODO Auto-generated method stub
-		return null;
+		return categoriaDao.save(categoria);
 	}
+
 
 }
