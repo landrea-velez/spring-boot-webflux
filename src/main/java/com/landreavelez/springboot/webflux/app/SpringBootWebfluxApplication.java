a6be2fcbf1af78ae0363ca5d031ee3b1f4da6265
landrea-velez <landrea.velez@gmail.com>
2025-06-09 09:12:17 -0500

    agregando una foto

--- src/main/java/com/landreavelez/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/landreavelez/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -42,26 +42,19 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 		Categoria computacion = new Categoria("Computación");
 		Categoria muebles = new Categoria("Muebles");
 
-		Flux.just(electronico, deporte, computacion, muebles).flatMap(service::saveCategoria) // c ->
-																								// service.saveCategoria(c)
-				.doOnNext(c -> {
-					log.info("Categoria creada: " + c.getNombre() + ", Id: " + c.getId());
-				})
-				.thenMany(Flux
-						.just(new Producto("TV Panasonic Pantalla LCD", 456.89, electronico),
-								new Producto("Sony Camara HD Digital", 177.89, electronico),
-								new Producto("Apple iPod", 46.89, electronico),
-								new Producto("Sony Notebook", 846.89, computacion),
-								new Producto("Hewlett Packard Multifuncional", 200.89, computacion),
-								new Producto("Bianchi Bicicleta", 70.89, deporte),
-								new Producto("HP Notebook Omen 17", 2500.89, computacion),
-								new Producto("Mica Cómoda 5 Cajones", 150.89, muebles),
-								new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89, electronico))
-						.flatMap(producto -> {
-							producto.setCreateAt(new Date());
-							return service.save(producto);
-						}))
-				.subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
+		Flux.just(electronico, deporte, computacion, muebles).flatMap(service::saveCategoria).doOnNext(c -> {
+			log.info("Categoria creada: " + c.getNombre() + ", Id: " + c.getId());
+		}).thenMany(Flux.just(new Producto("TV Panasonic Pantalla LCD", 456.89, electronico),
+				new Producto("Sony Camara HD Digital", 177.89, electronico),
+				new Producto("Apple iPod", 46.89, electronico), new Producto("Sony Notebook", 846.89, computacion),
+				new Producto("Hewlett Packard Multifuncional", 200.89, computacion),
+				new Producto("Bianchi Bicicleta", 70.89, deporte),
+				new Producto("HP Notebook Omen 17", 2500.89, computacion),
+				new Producto("Mica Cómoda 5 Cajones", 150.89, muebles),
+				new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89, electronico)).flatMap(producto -> {
+					producto.setCreateAt(new Date());
+					return service.save(producto);
+				})).subscribe(producto -> log.info("Insert: " + producto.getId() + " " + producto.getNombre()));
 
 	}
 }
